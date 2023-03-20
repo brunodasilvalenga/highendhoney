6bfe5454bfb451cef7c4ddcb10981fa52241b62d
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2023-03-21 00:51:32 +1000

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,10 +131,10 @@ const faqs = [
     question: "What's the best thing about Switzerland?",
     answer:
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
-  },
+  }
 ];
 
-function classNames(...classes) {
+function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 
